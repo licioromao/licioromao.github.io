739cf04c11ccf98cdb86a0a780be07a9186baa95
licio.romao <licio.romao@gmail.com>
2024-04-11 01:21:12 -0700

    Stanford version

--- assets/js/buttom_func.js
+++
@@ -1,8 +0,0 @@
-function toggleCode(id) {
-    var code = document.getElementById(id);
-    if (code.style.display === "none") {
-      code.style.display = "block";
-    } else {
-      code.style.display = "none";
-    }
-  }
